69156e6f5a5bd7c1fb6296195c301b55a7a739f8
Рыбалкин Виталий Михайлович <kinjeks@gmail.com>
2021-10-13 17:57:15 +0300

    Add database

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
 
@@ -11,36 +12,48 @@ function App() {
 
 
 
-  const fetchMoviesHandler = useCallback(async () => {{
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true)
     setError(null)
     try{
-     const response = await fetch('https://swapi.dev/api/films/')
+     const response = await fetch('https://react-http-66065-default-rtdb.firebaseio.com/movies.json')
 
       if(!response.ok){
         throw new Error('Something went wrong')
       }
        const data = await response.json()
+      let loadedMovies = []
 
-        const transformedMovies = data.results.map(movieData => {
-          return {
-            id:movieData.episode_id,
-            title:movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate:movieData.release_date
-          }
+      for (const key in data) {
+        loadedMovies.push({
+          id:key,
+          title:data[key].title,
+          openingText:data[key].openingText,
+          releaseDate:data[key].releaseDate,
         })
-    setMovies(transformedMovies)
+      }
+      console.log(loadedMovies);
+
+      setMovies(loadedMovies)
     }catch (error) {
       setError(error.message)
     }
     setIsLoading(false)
-}
   },[]);
 
-  useEffect(()=> {
-    fetchMoviesHandler()
-  },[fetchMoviesHandler])
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  function addMovieHandler(movie) {
+   fetch('https://react-http-66065-default-rtdb.firebaseio.com/movies.json',{
+      method:'POST',
+     body: JSON.stringify(movie),
+     headers:{
+        'Content-Type' : 'application/json'
+     }
+   })
+  }
 
 let content = <p>Movies not found</p>
 
@@ -57,6 +70,9 @@ if (movies.length > 0){
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
